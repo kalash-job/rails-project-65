9e93d501333819b3e4927cf6f19ca05d9552aa01
Nikolay Kalashnikov <n.kalashnikov@crpo.su>
2023-04-03 17:15:37 +0200

    add new tests to BulletinsControllerTest

--- test/controllers/web/bulletins_controller_test.rb
+++ test/controllers/web/bulletins_controller_test.rb
@@ -2,24 +2,20 @@
 
 require 'test_helper'
 
-class BulletinsControllerTest < ActionDispatch::IntegrationTest
+class Web::BulletinsControllerTest < ActionDispatch::IntegrationTest
+  UNDER_MODERATION_STATE = 'under_moderation'
+  DRAFT_STATE = 'draft'
+  ARCHIVED_STATE = 'archived'
+
   setup do
+    @user = users(:one)
     @bulletin = bulletins(:one)
     title = Faker::Lorem.paragraph_by_chars(number: 50)
     description = Faker::Lorem.paragraph_by_chars(number: 1000)
     category_id = categories(:one).id
-    @attrs = {
-      title:,
-      description:,
-      category_id:,
-      image: fixture_file_upload('image1.jpg', 'image/jpeg')
-    }
+    @attrs = { title:, description:, category_id:, image: fixture_file_upload('image1.jpg', 'image/jpeg') }
     @searching_attrs = { title:, description:, category_id: }
-  end
-
-  def after_teardown
-    super
-    FileUtils.rm_rf(ActiveStorage::Blob.service.root)
+    @another_user_bulletin = bulletins(:draft_bulletin)
   end
 
   test 'should get index' do
@@ -28,7 +24,7 @@ class BulletinsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test 'should get new' do
-    sign_in users(:one)
+    sign_in @user
     get new_bulletin_url
     assert_response :success
   end
@@ -39,7 +35,7 @@ class BulletinsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test 'should create bulletin' do
-    sign_in users(:one)
+    sign_in @user
     post bulletins_url, params: { bulletin: @attrs }
     bulletin = Bulletin.find_by(@searching_attrs.merge(user_id: current_user.id))
 
@@ -59,5 +55,100 @@ class BulletinsControllerTest < ActionDispatch::IntegrationTest
   test 'should show bulletin' do
     get bulletin_url(@bulletin)
     assert_response :success
+  end
+
+  test 'should get edit' do
+    sign_in @user
+    get edit_bulletin_url(@bulletin)
+    assert_response :success
+  end
+
+  test 'should not get edit for another user bulletin' do
+    sign_in @user
+    get edit_bulletin_url(@another_user_bulletin)
+    assert_redirected_to root_url
+  end
+
+  test 'should not get edit without login' do
+    get edit_bulletin_url(@bulletin)
+    assert_redirected_to root_url
+  end
+
+  test 'should update bulletin' do
+    sign_in @user
+    patch bulletin_url(@bulletin), params: { bulletin: @attrs }
+
+    bulletin = Bulletin.find_by(@searching_attrs)
+    assert { bulletin }
+    assert_redirected_to profile_url
+  end
+
+  test 'should not update another user bulletin' do
+    sign_in @user
+    patch bulletin_url(@another_user_bulletin), params: { bulletin: @attrs }
+
+    bulletin = Bulletin.find_by(@searching_attrs)
+    assert { !bulletin }
+    assert_redirected_to root_url
+  end
+
+  test 'should not update bulletin without login' do
+    patch bulletin_url(@bulletin), params: { bulletin: @attrs }
+
+    bulletin = Bulletin.find_by(@searching_attrs)
+    assert { !bulletin }
+    assert_redirected_to root_url
+  end
+
+  test 'should archive bulletin' do
+    sign_in @user
+    patch archive_bulletin_url(@bulletin)
+    @bulletin.reload
+
+    assert_redirected_to profile_path
+    assert { @bulletin.state == ARCHIVED_STATE }
+  end
+
+  test 'should not archive another user bulletin' do
+    sign_in(@user)
+    patch archive_bulletin_url(@another_user_bulletin)
+    @another_user_bulletin.reload
+
+    assert_redirected_to root_url
+    assert { @another_user_bulletin.state == DRAFT_STATE }
+  end
+
+  test 'should not archive bulletin without login' do
+    patch archive_bulletin_url(@bulletin)
+    @bulletin.reload
+
+    assert_redirected_to root_url
+    assert { @bulletin.state == DRAFT_STATE }
+  end
+
+  test 'should moderate bulletin' do
+    sign_in @user
+    patch moderate_bulletin_url(@bulletin)
+    @bulletin.reload
+
+    assert_redirected_to profile_path
+    assert { @bulletin.state == UNDER_MODERATION_STATE }
+  end
+
+  test 'should not moderate another user bulletin' do
+    sign_in(@user)
+    patch moderate_bulletin_url(@another_user_bulletin)
+    @another_user_bulletin.reload
+
+    assert_redirected_to root_url
+    assert { @another_user_bulletin.state == DRAFT_STATE }
+  end
+
+  test 'should not moderate bulletin without login' do
+    patch moderate_bulletin_url(@bulletin)
+    @bulletin.reload
+
+    assert_redirected_to root_url
+    assert { @bulletin.state == DRAFT_STATE }
   end
 end
