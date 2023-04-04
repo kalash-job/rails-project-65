59ffc3f2a797fbba15045f9e70de0445895a17b3
Nikolay Kalashnikov <n.kalashnikov@crpo.su>
2023-04-04 16:03:52 +0200

    delete unused with_attached_image

--- app/controllers/web/admin/admins_controller.rb
+++ app/controllers/web/admin/admins_controller.rb
@@ -2,6 +2,6 @@
 
 class Web::Admin::AdminsController < Web::Admin::ApplicationController
   def index
-    @bulletins = Bulletin.with_attached_image.under_moderation.by_creation_date_desc.page(params[:page])
+    @bulletins = Bulletin.under_moderation.by_creation_date_desc.page(params[:page])
   end
 end
