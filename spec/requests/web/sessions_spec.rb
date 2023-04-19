7d0bd4005101de6c39135ea148c85c369497a682
Nikolay Kalashnikov <n.kalashnikov@crpo.su>
2023-04-19 02:20:06 +0200

    add Web::Sessions spec

--- spec/requests/web/sessions_spec.rb
+++ spec/requests/web/sessions_spec.rb
@@ -3,7 +3,13 @@
 require 'rails_helper'
 
 RSpec.describe 'Web::Sessions', type: :request do
-  describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+  describe 'DELETE /destroy' do
+    fixtures :users
+    it 'signs out and redirects to root' do
+      sign_in users(:one)
+      delete session_url
+      expect(signed_in?).to be_falsey
+      expect(response).to redirect_to(root_url)
+    end
   end
 end
