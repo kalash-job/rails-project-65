f527979a649ed199e6ec877557661c9117f739a5
Nikolay Kalashnikov <n.kalashnikov@crpo.su>
2023-04-19 02:27:56 +0200

    add Web::Admin::Admins spec

--- spec/requests/web/admin/admins_spec.rb
+++ spec/requests/web/admin/admins_spec.rb
@@ -4,6 +4,28 @@ require 'rails_helper'
 
 RSpec.describe 'Web::Admin::Admins', type: :request do
   describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+    fixtures :users
+    context 'when user is admin' do
+      it 'renders a successful response' do
+        sign_in users(:admin)
+        get admin_root_url
+        expect(response).to be_successful
+      end
+    end
+
+    context 'when user is not admin' do
+      it 'redirects to root' do
+        sign_in users(:one)
+        get admin_root_url
+        expect(response).to redirect_to(root_url)
+      end
+    end
+
+    context 'when user is not signed in' do
+      it 'redirects to root' do
+        get admin_root_url
+        expect(response).to redirect_to(root_url)
+      end
+    end
   end
 end
